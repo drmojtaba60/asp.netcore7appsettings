82299614137049d5d5b303b4f605b061039894dc
shaghi <mojtaba.shagi@gmail.com>
2023-09-09 16:09:00 +0330

    IConfiguration in action

--- Asp.NetCore7AppSettings/Controllers/AppSettingsByOptionsController.cs
+++ Asp.NetCore7AppSettings/Controllers/AppSettingsByOptionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Asp.NetCore7AppSettings.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using System.Net.Mime;
 
@@ -11,10 +12,10 @@ namespace Asp.NetCore7AppSettings.Controllers
     {
        
 
-        [HttpGet("identity-config-ioptions2")]
+        [HttpGet("identity-config-ioptions")]
         [Produces(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IdentityConfiguration>> GetIdentityConfigByIoptins2(IOptions<IdentityConfiguration> identityConfigAsIOptions)
+        public async Task<ActionResult<IdentityConfiguration>> GetIdentityConfigByIoptins(IOptions<IdentityConfiguration> identityConfigAsIOptions)
         {
             var idnConfigs = identityConfigAsIOptions.Value ?? new IdentityConfiguration();
             var configs = await Task.FromResult(idnConfigs);
@@ -22,7 +23,16 @@ namespace Asp.NetCore7AppSettings.Controllers
             return Ok(configs);
         }
 
-
+        [HttpGet("identity-config-ioptions2")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IdentityConfiguration>> GetIdentityConfigByIoptins2(IConfiguration _configuration)
+        {
+            var passwordMinLength = _configuration.GetValue<int>("IdentityConfiguration:PasswordConfiguration:MinLength");
+            var userConfiguration = _configuration.GetSection("IdentityConfiguration:UserConfiguration");
+            var defaultPassword = userConfiguration.GetValue<string>("DefaultPassword");
+            return Ok(new { passwordMinLength, defaultPassword });
+        }
 
 
     }
